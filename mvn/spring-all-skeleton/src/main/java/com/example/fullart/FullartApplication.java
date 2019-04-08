516e93fcef40863bfdb43637515fd5a909501b99
Joseph Gan <josephganbw.st@gmail.com>
2019-04-08 09:02:34 +0800

    Add a skeleton project that includes (almost) all spring dependencies from start.spring.io

---
+++ mvn/spring-all-skeleton/src/main/java/com/example/fullart/FullartApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullartApplication.class, args);
+	}
+
+}
